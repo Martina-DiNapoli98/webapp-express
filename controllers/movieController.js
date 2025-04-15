a507cc083e32d390d1fdf8a62c6b3f7d64d9b636
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-04-15 17:33:42 +0200

    Implement movie retrieval in index and show functions

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,13 +1,34 @@
 const connection = require('../data/db')
 
 function index(req, res) {
-    res.json({ message: 'List of movies' })
+
+    const sql = 'SELECT * FROM movies'
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Query Failed' })
+        console.log(results)
+        res.json(results)
+    })
+
 };
 
 
 function show(req, res) {
-    const { id } = req.params
-    res.json({ message: `List of movies with id: ${id}` })
+    const id = Number(req.params.id)
+    const sql = 'SELECT * FROM movies WHERE id = ?'
+    const sqlReviews = 'SELECT * FROM reviews WHERE movie_id = ?'
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: err.message })
+        if (results.length === 0) return res.status(404).json({ error: 'Movies not found' })
+        const movie = results[0]
+
+        connection.query(sqlReviews, [id], (err, reviews) => {
+            if (err) return res.status(500).json({ error: err.message })
+            movie.reviews = reviews
+            console.log(movie.reviews)
+            res.json(movie)
+        })
+
+    });
 };
 
 module.exports = { index, show }
